c5a0b6832d19037140529f601bf2f7871f79a974
mattmatt91 <matthias_muhr@web.de>
2023-03-08 21:45:30 +0100

    _

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import time
 import json
 import serial
-from pprint import pprint
 import random
 import numpy as np
 
@@ -23,7 +22,8 @@ if __name__ == "__main__":
 
     values_x = []
     values_y = []
-    last_x = 0
+    offset_x = 0
+    offset_y = 0
 
 
 
@@ -32,24 +32,29 @@ if __name__ == "__main__":
             time.sleep(0.1)
             try:
                 incoming = ser.readline().decode("utf-8")
-                if len(incoming) >0:
-                    data = json.loads(incoming)
-                    if 'Temperature' in data.keys():
-                        print(data)
-                    else:
-                        last_x = data['X']
-                        # print(data['X'], (int(last_x)//10)*' ', end="\r")
-                        offset_x = 0
-                        value_x = (int(data['X'])//10)
-                        print(data['X'], value_x*'#'+ (100-value_x)*' ' , end="\r")
-                        values_x.append(data['X'])
-                        if len(values_x)>10:
-                            values_x = values_x[len(values_x)-10:]
-                        # print(np.mean(values_x))
- 
+                try:
+                    if len(incoming) >0:
+                        data = json.loads(incoming)
+                        if 'Temperature' in data.keys():
+                            print(data)
+                        else:
+                            value_x = np.abs(int(data['X']))-offset_x
+                            values_x.append(data['X'])
+                            
+                            value_y = np.abs(int(data['Y']))-offset_y
+                            values_y.append(value_y)
+                        
+                            if len(values_x)>10:
+                                values_x = values_x[len(values_x)-10:]
+                            if len(values_y)>10:
+                                values_y = values_y[len(values_y)-10:]
+                            print(np.mean(values_x), np.mean(values_y), value_x, value_y)
+                except:
+                    pass
+                    print(incoming)
+                    
             except Exception as e:
                 print (e)
-                print('reset')
                 ser.close()
-                time.sleep(2)
-                ser = start_serial()
+                exit()
+              
